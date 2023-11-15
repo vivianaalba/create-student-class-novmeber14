52ac42d77565ed5d81f66e33f4e8c75282555c8e
Viviana Alba <141361486+vivianaalba@users.noreply.github.com>
2023-11-14 18:15:58 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor (name, age, major) {
+      this.name = name;
+      this.age = age;
+      this.major = major;
+    }
+  }
+  
+  let viviana = new Student ("Viviana Alba", 24, "Biology");
+  let nikki = new Student ("Nikki Keyvan", 25, "Nueroscience");
+  console.log(viviana);
+  console.log(nikki);
